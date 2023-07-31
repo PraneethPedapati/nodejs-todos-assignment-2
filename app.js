262447832f6d6a0d0a52c6d20a30f841d3f759a1
PraneethPedapati <praneethpedapati98@gmail.com>
2023-07-31 09:36:45 +0000

    API-1 added

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express();
 const { open } = require("sqlite");
 const sqlite3 = require("sqlite3");
 const path = require("path");
+const bcrypt = require("bcrypt");
 
 app.use(express.json());
 
@@ -23,3 +24,32 @@ const initiateDBAndServer = async () => {
 };
 
 initiateDBAndServer();
+
+app.post("/register", async (req, res) => {
+  const userDetails = req.body;
+  const { username, password, gender, name } = userDetails;
+
+  let sql = `SELECT * FROM user WHERE username = '${username}'`;
+
+  let user = await db.get(sql);
+
+  if (user) {
+    res.status(400).send("User already exists");
+  } else {
+    if (password.length < 6) {
+      res.status(400).send("Password is too short");
+    } else {
+      let encryptedPassword = await bcrypt.hash(req.body.password, 10);
+      let insertSql = `INSERT INTO user (username, password, name, gender)
+        VALUES (
+            '${username}',
+            '${encryptedPassword}',
+            '${name}',
+            '${gender}'
+            )`;
+
+      let newUser = await db.run(insertSql);
+      res.send("Successful registration of the registrant");
+    }
+  }
+});
